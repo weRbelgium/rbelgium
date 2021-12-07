a205f477db26f9ab34240d6cb246b3c2b56e167b
Jan Wijffels <jwijffels@bnosac.be>
2021-12-07 09:49:42 +0100

    make sure RSS feed has full site

--- deploy-content.sh
+++ deploy-content.sh
@@ -1,8 +1,8 @@
 ## Build and 
 
-hugo --buildDrafts
-sudo rm -r /var/www/rbelgium
-sudo mkdir /var/www/rbelgium
+hugo --buildDrafts --baseUrl=https://www.rbelgium.be
+#sudo rm -r /var/www/rbelgium
+#sudo mkdir /var/www/rbelgium
 sudo cp -a public/* /var/www/rbelgium/
 sudo chown -R www-data:www-data /var/www/rbelgium/ 
 
